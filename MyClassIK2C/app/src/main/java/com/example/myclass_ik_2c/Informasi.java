d316cf07e37914b7941094fa6d45dcef78696f68
andriyan-fikri <fikrinuraga@gmail.com>
2020-01-05 20:51:15 +0700

    first commit

---
+++ MyClassIK2C/app/src/main/java/com/example/myclass_ik_2c/Informasi.java
@@ -0,0 +1,15 @@
+package com.example.myclass_ik_2c;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Informasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informasi);
+    }
+}
